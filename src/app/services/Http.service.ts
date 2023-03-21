dab14703175b2794e43f67f0b3b1cd5e3a4aef5a
Bas <baskruithof999@gmail.com>
2023-03-21 08:55:29 +0100

    Restructed project files agaun, previous structure didn't make sense

--- src/app/services/Http.service.ts
+++ src/app/services/Http.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { LoginComponent } from "../app/login/login.component";
+import { LoginComponent } from "../login/login.component";
 
 @Injectable({
   providedIn: 'root'
